e46a066f1ab72c9fa702c7bb4e28d2ca3c2e7417
orussmann <665455453+orussmann@users.noreply.github.com>
2023-10-24 20:00:31 +0200

    Add ability to play 5 rounds against the computer

--- script.js
+++ script.js
@@ -4,5 +4,34 @@ function getComputerChoice() {
     return choices[choicesIndex];
 }
 
+/* TODO:
+    - check for unexpected/ incorrect input, like a number or a word that's not a defined choice
+*/
+function playRound(playerSelection, computerSelection) {
+    playerSelection = String(playerSelection).toLowerCase();
+    computerSelection = String(computerSelection).toLowerCase();
+    let rock = "rock";
+    let paper = "paper";
+    let scissors = "scissors";
 
+    if (playerSelection === computerSelection) {
+        return `Draw! ${playerSelection} can't beat ${playerSelection}`;
+    } else if (playerSelection === rock && computerSelection === scissors
+        || playerSelection === paper && computerSelection === rock
+        || playerSelection === scissors && computerSelection === paper) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+
+// TODO: When Input is Empty string, user shouldnt automatically lose.
+function game() {
+    for (let i = 0; i < 5 ; i++) {
+        console.log(playRound(prompt("Enter one of the options: Rock, Paper or Scissors."), getComputerChoice()));
+    }
+}
+
+game();
 
